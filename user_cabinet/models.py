6c5334e9d362d5c8a1358bd0533068927f254731
ST1LLY <st1lly@protonmail.com>
2022-07-07 20:02:00 +0400

    refactor: modify model

--- user_cabinet/models.py
+++ user_cabinet/models.py
@@ -16,9 +16,6 @@ class Domain(models.Model):
         """
         Statuses for process
         """
-
-        # waiting of registration software in domain after adding domain in list
-        WAIT_SOFT_ADDING = 'WAIT_SOFT_ADDING'
         # initial status after successful registration in domain
         INIT = 'INIT'
         # waiting of start a process
@@ -30,33 +27,33 @@ class Domain(models.Model):
         # if an error occurs during a process
         ERROR = 'ERROR'
 
-    name: Any = models.CharField(max_length=255, unique=True, null=False)
-    hostname: Any = models.CharField(max_length=255, unique=True, null=False)
-    base_dn: Any = models.CharField(max_length=255, unique=True, null=False)
-    workstation_name: Any = models.CharField(max_length=15, null=True)
-    workstation_password: Any = models.CharField(max_length=128, null=True)
-    user_dn: Any = models.CharField(max_length=255, unique=True, null=False)
-    user_password: Any = models.CharField(max_length=128, null=True)
+    name: Any = models.CharField(max_length=255, null=False)
+    hostname: Any = models.CharField(max_length=255, null=False)
+    base_dn: Any = models.CharField(max_length=255, null=False)
+    workstation_name: Any = models.CharField(max_length=15, null=False)
+    workstation_password: Any = models.CharField(max_length=128, null=False)
+    user_dn: Any = models.CharField(max_length=255, null=False)
+    user_password: Any = models.CharField(max_length=128, null=False)
 
     dump_status: Any = models.CharField(
-        max_length=20, default=ProcessStatus.WAIT_SOFT_ADDING, choices=ProcessStatus.choices
+        max_length=20, default=ProcessStatus.INIT, choices=ProcessStatus.choices
     )
     dump_status_update: Any = models.DateTimeField(default=datetime.datetime(1970, 1, 1))
     dump_err_desc: Any = models.TextField(null=True)
 
     brute_status: Any = models.CharField(
-        max_length=20, default=ProcessStatus.WAIT_SOFT_ADDING, choices=ProcessStatus.choices
+        max_length=20, default=ProcessStatus.INIT, choices=ProcessStatus.choices
     )
     brute_progress: Any = models.PositiveSmallIntegerField(default=0)
     brute_status_update: Any = models.DateTimeField(default=datetime.datetime(1970, 1, 1))
     brute_error_desc: Any = models.TextField(null=True)
     no_exp_pass_status: Any = models.CharField(
-        max_length=20, default=ProcessStatus.WAIT_SOFT_ADDING, choices=ProcessStatus.choices
+        max_length=20, default=ProcessStatus.INIT, choices=ProcessStatus.choices
     )
     no_exp_pass_status_update: Any = models.DateTimeField(default=datetime.datetime(1970, 1, 1))
     no_exp_pass_err_desc: Any = models.TextField(null=True)
     reused_pass_status: Any = models.CharField(
-        max_length=20, default=ProcessStatus.WAIT_SOFT_ADDING, choices=ProcessStatus.choices
+        max_length=20, default=ProcessStatus.INIT, choices=ProcessStatus.choices
     )
     reused_pass_status_update: Any = models.DateTimeField(default=datetime.datetime(1970, 1, 1))
     reused_pass_err_desc: Any = models.TextField(null=True)
