f4c457ff0d1aa7323f4bad3197c38c7c77a0b36d
ST1LLY <st1lly@protonmail.com>
2022-07-19 17:49:30 +0400

    refactor: update default params of blank settings

--- dc_sonar_web/sensitive_settings_blank.py
+++ dc_sonar_web/sensitive_settings_blank.py
@@ -5,14 +5,14 @@ Copy the file to sensitive_settings.py and fill in params
 S_DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '***',
-        'USER': '***',
-        'PASSWORD': '***',
-        'HOST': '***',
-        'PORT': '',
+        'NAME': 'web_app_db',
+        'USER': 'dc_sonar_user_layer',
+        'PASSWORD': '******',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-S_SECRET_KEY = ''
-S_SIGNING_KEY = ''
-S_AES_256_KEY = ''
+S_SECRET_KEY = '******'
+S_SIGNING_KEY = '******'
+S_AES_256_KEY = '******'
